297b475f0daebc452f676f53c73f845bc0609396
Bas Kraai <bas@kraai.email>
2022-04-19 11:24:57 +0200

    Bugfix: do not add permissions to user

--- hetzner.tf
+++ hetzner.tf
@@ -1,19 +1,30 @@
-resource "aws_iam_user" "iac-hetzner-dev" {
+resource "aws_iam_user" "iac-hetzner-dev_pipeline-user" {
   name = "iac-hetzner-dev"
 }
 
-resource "aws_iam_access_key" "iac-hetzner-dev" {
-  user = aws_iam_user.iac-hetzner-dev.name
+resource "aws_iam_access_key" "iac-hetzner-dev_pipeline_creds" {
+  user = aws_iam_user.iac-hetzner-dev_pipeline-user.name
 }
 
-resource "tfe_variable" "iac-hetzner-dev-aws-secret" {
-  key          = "aws_secret"
-  value        = aws_iam_access_key.iac-hetzner-dev.secret
-  category     = "terraform"
-  workspace_id = tfe_workspace.iac-hetzner-dev.id
+resource "aws_iam_user_policy" "iac-hetzner-dev_iam_assume_policy" {
+  name = "iac-hetzner-dev-secret-role_assume"
+  user = aws_iam_user.iac-hetzner-dev_pipeline-user.name
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "role_iac-hetzner-dev-secret",
+            "Effect": "Allow",
+            "Action": "sts:AssumeRole",
+            "Resource": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${aws_iam_role.iac-hetzner-dev_role.name}"
+        }
+    ]
+}
+EOF
 }
 
-resource "aws_iam_policy" "iac-hetzner-dev-secret" {
+resource "aws_iam_policy" "iac-hetzner-dev_role_policies" {
   name        = "iac-hetzner-dev"
   policy      = jsonencode({
       "Version": "2012-10-17",
@@ -32,7 +43,7 @@ resource "aws_iam_policy" "iac-hetzner-dev-secret" {
                   "secretsmanager:TagResource",
                   "secretsmanager:UpdateSecret"
               ],
-              "Resource": "arn:aws:secretsmanager:eu-central-1:${aws_caller_identity.current.account_id}:secret:iac-hetzner-dev"
+              "Resource": "arn:aws:secretsmanager:eu-central-1:${data.aws_caller_identity.current.account_id}:secret:iac-hetzner-dev"
           },
           {
               "Sid": "VisualEditor1",
@@ -44,19 +55,45 @@ resource "aws_iam_policy" "iac-hetzner-dev-secret" {
   })
 }
 
-resource "aws_iam_user_policy_attachment" "iac-hetzner-dev-aws-secret" {
-  user       = aws_iam_user.iac-hetzner-dev.name
-  policy_arn = aws_iam_policy.iac-hetzner-dev-secret.arn
+resource "aws_iam_role" "iac-hetzner-dev_role" {
+  name = "iac-hetzner-dev-secret"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal: {
+            AWS: "570931902845"
+        }
+      },
+    ]
+  })
 }
+
+resource "aws_iam_role_policy_attachment" "iac-hetzner-dev_policy-role-attach" {
+  role       = aws_iam_role.iac-hetzner-dev_role.name
+  policy_arn = aws_iam_policy.iac-hetzner-dev_role_policies.arn
+}
+
 
 data "tfe_workspace" "iac-hetzner-dev" {
   name         = "iac-hetzner-dev"
-  organization = tfe_organizations.cloud.names[0]
+  organization = data.tfe_organizations.cloud.names[0]
 }
 
 resource "tfe_variable" "iac-hetzner-dev-aws-key" {
   key          = "aws_key"
-  value        = aws_iam_access_key.iac-hetzner-dev.id
+  value        = aws_iam_access_key.iac-hetzner-dev_pipeline_creds.id
   category     = "terraform"
-  workspace_id = tfe_workspace.iac-hetzner-dev.id
+  workspace_id = data.tfe_workspace.iac-hetzner-dev.id
+}
+
+resource "tfe_variable" "iac-hetzner-dev-aws-secret" {
+  key          = "aws_secret"
+  value        = aws_iam_access_key.iac-hetzner-dev_pipeline_creds.secret
+  category     = "terraform"
+  workspace_id = data.tfe_workspace.iac-hetzner-dev.id
 }
